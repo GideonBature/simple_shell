8395bfd059fe2ef28ff965c9d5648546f0e27634
wrightkhlebisol <calebogundiya@gmail.com>
2023-07-20 17:32:49 +0100

    remove value at key

--- linkedlist.c
+++ linkedlist.c
@@ -22,10 +22,42 @@ char *get_value(envstruct *head, char *key)
 	if (head == NULL)
                 return (NULL);
 
+	if ((head->next == NULL) && (head->key == key))
+		return (head->value);
+
 	while (head->next != NULL)
 	{
 		if (head->key == key)
 			return (head->value);
-		head++;
+		head = head->next;
 	}
+
+	return (NULL);
+}
+
+int remove_value(envstruct *head, char *key)
+{
+	envstruct *previous = NULL, *current = NULL, *next_node = NULL;
+
+	if (head == NULL)
+                return (NULL);
+
+	if ((head->next == NULL) && (head->key == key))
+	{
+		head = NULL;
+		return (0);
+	}
+
+	while (head->next != NULL)
+        {
+                if (head->key == key)
+		{
+			previous->next = next_node
+			current->next = NULL;
+                        return (0);
+		}
+		previous = head;
+		head = head->next;
+        }
+	return (1);
 }
